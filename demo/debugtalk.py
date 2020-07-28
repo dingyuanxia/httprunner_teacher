52ef299e60615101381eaece3a6cd917e38823aa
gerrit <hwmxrhx@126.com>
2020-07-28 11:11:10 +0800

    老师代码

---
+++ demo/debugtalk.py
@@ -0,0 +1,22 @@
+import time
+
+from httprunner import __version__
+
+
+def get_httprunner_version():
+    return __version__
+
+
+def sum_two(m, n):
+    return m + n
+
+
+def sleep(n_secs):
+    time.sleep(n_secs)
+
+def get_memberId():
+    return "5990107260590036"
+
+
+def get_timetamp():
+    return int(time.time() * 1000)
